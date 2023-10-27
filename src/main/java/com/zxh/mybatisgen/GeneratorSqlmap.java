e3a64073c5bb252cc899de96d52b31c91b667bfb
omicron <2405528539@qq.com>
2023-10-27 15:07:10 +0800

    Initial commit

---
+++ src/main/java/com/zxh/mybatisgen/GeneratorSqlmap.java
@@ -0,0 +1,39 @@
+package com.zxh.mybatisgen;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorSqlmap {
+
+    public void generator() throws Exception{
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        // 换成自己的位置
+
+        File configFile = new File("E:\\项目\\22\\mybatisgen\\src\\main\\resources\\generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+    public static void main(String[] args) throws Exception {
+        try {
+            GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
